e95df82e5208071418ea5db0af4d5a3d28a90e0e
kosynka <sayat.kaldarbekov.00@gmail.com>
2022-07-13 21:41:47 +0600

    meta added

--- components/layout.js
+++ components/layout.js
@@ -8,6 +8,12 @@ const Layout = (props) => {
                 <title>{props.pageTitle}</title>
                 <meta charSet='UTF-8'/>
                 <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
+                <meta property="og:title" content="Чистка ковров | kilem juu"/>
+                <meta property="og:site_name" content="kilem juu"/>
+                <meta property="og:url" content="https://carpet-landing-on-next.vercel.app/"/>
+                <meta property="og:description" content=""/>
+                <meta property="og:locale" content="ru_KZ" />
+                <meta property="og:image" content="/images/welcome_page.jpg"/>
                 <link
                     rel='stylesheet'
                     href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css'
